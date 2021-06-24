d0d9b63dfd542be8e11425cf55b475a8281b94ee
Eriberto Mendz <erimendz@gmail.com>
2021-06-24 11:41:53 +0300

    add if/else conditional codes

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+// all about conditionals from book Rust by Example
+
+// if/else
+
 fn main() {
-    println!("Hello, world!");
+    let n = 500;
+    if n < 0 {
+        println!("{} is negative!", n);
+    } else if n > 0 {
+        println!("{} is positive!", n);
+    } else {
+        println!("{} is zero!", n);
+    }
+
+    let big_n = if n < 10 && n > 0 {
+        println!("{} is a small number so increase ten-folds!", n);
+        n * 10
+    } else {
+        println!("{} is a big number so cut in half!", n);
+        n / 2
+    }; // all let bindings need closing semicolon
+    println!("{} -> {}", n, big_n);
 }
